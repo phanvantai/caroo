e8ff313c42b283ea7e0a7d26833d010d8f867e85
Tai Phan Van <taipv.swe@gmail.com>
2025-01-14 21:06:38 +0700

    Initial Commit

---
+++ CarooTests/CarooTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarooTests.swift
+//  CarooTests
+//
+//  Created by Tai Phan Van on 14/1/25.
+//
+
+import Testing
+@testable import Caroo
+
+struct CarooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
